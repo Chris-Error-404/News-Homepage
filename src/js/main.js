09408d5e7925cb69da351d8a2add87f88ec1ea2c
Nzenwata Christopher <nzenwatachristopher186@gmail.com>
2025-06-27 18:03:46 +0100

    Deployment test

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+//HAMBURGER MENU SCRIPT
+const btn = document.getElementById('menu-btn');
+const nav = document.getElementById('menu');
+
+btn.addEventListener('click', () => {
+  btn.classList.toggle('open')
+  nav.classList.toggle('open')
+  nav.classList.toggle('hidden')
+})
