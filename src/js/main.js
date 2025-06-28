43454f71e177e71f01984c8ef535714012eec113
Nzenwata Christopher <nzenwatachristopher186@gmail.com>
2025-06-28 23:15:34 +0100

    Done

--- src/js/main.js
+++ src/js/main.js
@@ -5,5 +5,5 @@ const nav = document.getElementById('menu');
 btn.addEventListener('click', () => {
   btn.classList.toggle('open')
   nav.classList.toggle('open')
-  nav.classList.toggle('hidden')
+  nav.classList.toggle('close')
 })
